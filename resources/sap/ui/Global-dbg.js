c8bc8e9a4b02a9bbe60b03238560b680cd5abea8
AmirthavalliG <AmirthavalliG@users.noreply.github.com>
2024-02-16 07:33:21 +0000

    deploy: 1b155f4830510c132fd8587f6fc9654f03361f93

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -77,7 +77,7 @@ sap.ui.define([
 		 */
 		version: "1.120.6",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20240216-0729" }
+		buildinfo : { lastchange : "", buildtime : "20240216-0732" }
 	});
 
 	var syncCallBehavior = sap.ui.loader._.getSyncCallBehavior();
